d91420ed9d39c414860940c27f7c823d47154192
nishant <nishant@devtron.ai>
2019-12-08 15:25:25 +0530

    debug log

--- ArtefactUploadUtil.go
+++ ArtefactUploadUtil.go
@@ -16,6 +16,11 @@ func UploadArtifact(artifactFiles map[string]string, s3Location string) error {
 	if err != nil {
 		return err
 	}
+	cmd1 := exec.Command("pwd")
+	cmd2 := exec.Command("ls")
+	RunCommand(cmd1)
+	RunCommand(cmd2)
+
 	for key, val := range artifactFiles {
 		loc := filepath.Join(tmpArtifactLocation, key)
 		err := os.Mkdir(loc, os.ModeDir)
@@ -33,6 +38,7 @@ func UploadArtifact(artifactFiles map[string]string, s3Location string) error {
 	if err != nil {
 		return err
 	}
+	RunCommand(cmd2)
 	log.Println(devtron, " artifact upload to ", zipFile, s3Location)
 	artifactPush := exec.Command("aws", "s3", "cp", zipFile, s3Location)
 
