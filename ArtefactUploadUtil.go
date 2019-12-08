5b252e6ed7e80db063331911bbcd926d3c6c90d7
nishant <nishant@devtron.ai>
2019-12-08 15:46:05 +0530

    debug log

--- ArtefactUploadUtil.go
+++ ArtefactUploadUtil.go
@@ -11,17 +11,16 @@ import (
 var tmpArtifactLocation = "./job-artifact"
 
 func UploadArtifact(artifactFiles map[string]string, s3Location string) error {
+	if len(artifactFiles) == 0 {
+		log.Println(devtron, "no artifact to upload")
+		return nil
+	}
 	//collect in a dir
 	log.Println(devtron, "artifact upload ", artifactFiles, s3Location)
 	err := os.Mkdir(tmpArtifactLocation, os.ModeDir)
 	if err != nil {
 		return err
 	}
-	cmd1 := exec.Command("pwd")
-	cmd2 := exec.Command("ls")
-	RunCommand(cmd1)
-	RunCommand(cmd2)
-
 	for key, val := range artifactFiles {
 		loc := filepath.Join(tmpArtifactLocation, key)
 		err := os.Mkdir(loc, os.ModeDir)
@@ -39,16 +38,13 @@ func UploadArtifact(artifactFiles map[string]string, s3Location string) error {
 	if err != nil {
 		return err
 	}
-	RunCommand(cmd2)
 	log.Println(devtron, " artifact upload to ", zipFile, s3Location)
 	artifactPush := exec.Command("aws", "s3", "cp", zipFile, s3Location)
-
-	tail := exec.Command("/bin/sh", "-c", "tail -f /dev/null")
-	err = RunCommand(tail)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
+	/*	tail := exec.Command("/bin/sh", "-c", "tail -f /dev/null")
+		err = RunCommand(tail)
+		if err != nil {
+			log.Println(err)
+			return err
+		}*/
 	return RunCommand(artifactPush)
 }
