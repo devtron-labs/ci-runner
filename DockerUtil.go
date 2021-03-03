f23baf1351560de9b7668a1c69c309304ea66f82
nishant <nishant@devtron.ai>
2021-03-03 16:57:32 +0530

    host parsin

--- DockerUtil.go
+++ DockerUtil.go
@@ -141,7 +141,7 @@ func BuildArtifact(ciRequest *CiRequest) (string, error) {
 			return "", err
 		}
 		u.Path = path.Join(u.Path, ciRequest.DockerRepository)
-		dockerRegistryURL := strings.TrimPrefix(strings.TrimPrefix(u.String(), u.Scheme), "//")
+		dockerRegistryURL := u.Host + u.Path
 		dest = dockerRegistryURL + ":" + ciRequest.DockerImageTag
 	}
 
