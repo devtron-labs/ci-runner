9c7abab6cb4b8eff2f6887fcba4dd591934705e1
nishant <nishant@devtron.ai>
2022-04-01 12:27:49 +0530

    bash executor with test case

--- StageExecutor.go
+++ StageExecutor.go
@@ -18,13 +18,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
-
 
 func RunPreDockerBuildTasks(ciRequest *CiRequest, scriptEnvs map[string]string, taskYaml *TaskYaml) error {
 	//before task
@@ -35,7 +36,7 @@ func RunPreDockerBuildTasks(ciRequest *CiRequest, scriptEnvs map[string]string, 
 		log.Println(devtron, "pre", task)
 		//log running cmd
 		logStage(task.Name)
-		err := RunScripts(output_path, fmt.Sprintf("before-%d", i), task.Script, scriptEnvs)
+		_, err := RunScripts(output_path, fmt.Sprintf("before-%d", i), task.Script, scriptEnvs, nil)
 		if err != nil {
 			return err
 		}
@@ -58,7 +59,7 @@ func RunPreDockerBuildTasks(ciRequest *CiRequest, scriptEnvs map[string]string, 
 		log.Println(devtron, "pre - yaml", task)
 		//log running cmd
 		logStage(task.Name)
-		err = RunScripts(output_path, fmt.Sprintf("before-yaml-%d", i), task.Script, scriptEnvs)
+		_, err = RunScripts(output_path, fmt.Sprintf("before-yaml-%d", i), task.Script, scriptEnvs, nil)
 		if err != nil {
 			return err
 		}
@@ -74,7 +75,7 @@ func RunPostDockerBuildTasks(ciRequest *CiRequest, scriptEnvs map[string]string,
 		afterTaskMap[task.Name] = task
 		log.Println(devtron, "post", task)
 		logStage(task.Name)
-		err := RunScripts(output_path, fmt.Sprintf("after-%d", i), task.Script, scriptEnvs)
+		_, err := RunScripts(output_path, fmt.Sprintf("after-%d", i), task.Script, scriptEnvs, nil)
 		if err != nil {
 			return err
 		}
@@ -96,7 +97,7 @@ func RunPostDockerBuildTasks(ciRequest *CiRequest, scriptEnvs map[string]string,
 		log.Println(devtron, "post - yaml", task)
 		//log running cmd
 		logStage(task.Name)
-		err = RunScripts(output_path, fmt.Sprintf("after-yaml-%d", i), task.Script, scriptEnvs)
+		_, err = RunScripts(output_path, fmt.Sprintf("after-yaml-%d", i), task.Script, scriptEnvs, nil)
 		if err != nil {
 			return err
 		}
@@ -116,7 +117,7 @@ func RunCdStageTasks(tasks []*Task, scriptEnvs map[string]string) error {
 		taskMap[task.Name] = task
 		log.Println(devtron, "stage", task)
 		logStage(task.Name)
-		err := RunScripts(output_path, fmt.Sprintf("stage-%d", i), task.Script, scriptEnvs)
+		_, err := RunScripts(output_path, fmt.Sprintf("stage-%d", i), task.Script, scriptEnvs, nil)
 		if err != nil {
 			return err
 		}
@@ -124,33 +125,27 @@ func RunCdStageTasks(tasks []*Task, scriptEnvs map[string]string) error {
 	return nil
 }
 
-func RunScripts(outputPath string, bashScript string, script string, envVars map[string]string) error {
+func RunScripts(workDirectory string, scriptFileName string, script string, envInputVars map[string]string, outputVars []string) (map[string]string, error) {
 	log.Println("running script commands")
-	scriptTemplate := `#!/bin/sh
-{{ range $key, $value := .envVr }}
-export {{ $key }}={{ $value }} ;
-{{ end }}
-{{.script}}
-`
+	envOutFileName := filepath.Join(workDirectory, fmt.Sprintf("%s_out.env", scriptFileName))
 
-	templateData := make(map[string]interface{})
-	templateData["envVr"] = envVars
-	templateData["script"] = script
-	finalScript, err := Tprintf(scriptTemplate, templateData)
+	//------------
+	finalScript, err := prepareFinaleScript(script, outputVars, envOutFileName)
 	if err != nil {
 		log.Println(devtron, err)
-		return err
+		return nil, err
 	}
-	err = os.MkdirAll(outputPath, os.ModePerm|os.ModeDir)
+	//--------------
+	err = os.MkdirAll(workDirectory, os.ModePerm|os.ModeDir)
 	if err != nil {
 		log.Println(devtron, err)
-		return err
+		return nil, err
 	}
-	scriptPath := filepath.Join(outputPath, bashScript)
+	scriptPath := filepath.Join(workDirectory, scriptFileName)
 	file, err := os.Create(scriptPath)
 	if err != nil {
 		log.Println(err)
-		return err
+		return nil, err
 	}
 	defer file.Close()
 	_, err = file.WriteString(finalScript)
@@ -158,14 +153,84 @@ export {{ $key }}={{ $value }} ;
 	log.Println(devtron, scriptPath)
 	if err != nil {
 		log.Println(devtron, err)
-		return err
+		return nil, err
+	}
+	var inputEnvironmentVariable []string
+	for k, v := range envInputVars {
+		inputEnvironmentVariable = append(inputEnvironmentVariable, fmt.Sprintf("%s=%s", k, v))
 	}
 
 	runScriptCMD := exec.Command("/bin/sh", scriptPath)
+	runScriptCMD.Env = inputEnvironmentVariable
 	err = RunCommand(runScriptCMD)
 	if err != nil {
 		log.Println(err)
-		return err
+		return nil, err
 	}
-	return nil
+	envMap, err := readEnvironmentFile(envOutFileName)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return envMap, nil
+}
+
+//prepare final shell script to be executed
+func prepareFinaleScript(script string, outputVars []string, envOutFileName string) (string, error) {
+	scriptTemplate := `
+#!/bin/sh
+set -e
+set -o pipefail
+{{.script}}
+> {{.envOutFileName}}
+{{$envOutFileName := .envOutFileName}}
+{{range .outputVars}} 
+  printf "\n{{.}}=%s" "${{.}}" >> {{$envOutFileName}}
+{{end}}
+`
+	templateData := make(map[string]interface{})
+	templateData["script"] = script
+	templateData["outputVars"] = outputVars
+	templateData["envOutFileName"] = envOutFileName
+	finalScript, err := Tprintf(scriptTemplate, templateData)
+	if err != nil {
+		return "", err
+	}
+	return finalScript, nil
+}
+
+//read content of .env file
+func readEnvironmentFile(filename string) (envMap map[string]string, err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err = scanner.Err(); err != nil {
+		return nil, err
+	}
+	envMap = parseEnvironmentFileLines(lines)
+	return envMap, nil
+}
+func parseEnvironmentFileLines(lines []string) map[string]string {
+	envMap := make(map[string]string)
+	for _, fullLine := range lines {
+		if !isIgnoredLine(fullLine) {
+			parts := strings.SplitN(fullLine, "=", 2)
+			if len(parts) != 2 {
+				continue
+			}
+			envMap[strings.TrimSpace(parts[0])] = strings.TrimSpace(parts[1])
+		}
+	}
+	return envMap
+}
+func isIgnoredLine(line string) bool {
+	trimmedLine := strings.TrimSpace(line)
+	return len(trimmedLine) == 0 || strings.HasPrefix(trimmedLine, "#")
 }
