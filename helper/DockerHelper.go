2aa7af4a6ae68d57b9aea78deb759619df7d20bc
kripanshdevtron <kripansh@devtron.ai>
2022-07-15 14:12:10 +0530

    adding network host flag

--- helper/DockerHelper.go
+++ helper/DockerHelper.go
@@ -199,7 +199,7 @@ func BuildArtifact(ciRequest *CiRequest) (string, error) {
 			return "", err
 		}
 
-		multiPlatformCmd = "docker buildx create --use"
+		multiPlatformCmd = "docker buildx create --use --buildkitd-flags '--allow-insecure-entitlement network.host'"
 		log.Println(" -----> " + multiPlatformCmd)
 		dockerBuildCMD = exec.Command("/bin/sh", "-c", multiPlatformCmd)
 		err = util.RunCommand(dockerBuildCMD)
@@ -247,7 +247,7 @@ func BuildArtifact(ciRequest *CiRequest) (string, error) {
 		}
 		oldCacheBuildxPath = oldCacheBuildxPath + "/cache"
 		manifestLocation := util.LOCAL_BUILDX_LOCATION + "/manifest.json"
-		dockerBuild = fmt.Sprintf("%s -f %s -t %s --push . --cache-to=type=local,dest=%s,mode=max --cache-from=type=local,src=%s --metadata-file %s", dockerBuild, ciRequest.DockerFileLocation, dest, localCachePath, oldCacheBuildxPath, manifestLocation)
+		dockerBuild = fmt.Sprintf("%s -f %s --network host -t %s --push . --cache-to=type=local,dest=%s,mode=max --cache-from=type=local,src=%s --metadata-file %s", dockerBuild, ciRequest.DockerFileLocation, dest, localCachePath, oldCacheBuildxPath, manifestLocation)
 	} else {
 		dockerBuild = fmt.Sprintf("%s -f %s --network host -t %s .", dockerBuild, ciRequest.DockerFileLocation, ciRequest.DockerRepository)
 	}
