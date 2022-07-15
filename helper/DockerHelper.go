24f49f35e80ab6fc8cb37400bee8281dc91a515a
kripanshdevtron <kripansh@devtron.ai>
2022-07-15 19:26:50 +0530

    buildx inspection removed

--- helper/DockerHelper.go
+++ helper/DockerHelper.go
@@ -208,14 +208,14 @@ func BuildArtifact(ciRequest *CiRequest) (string, error) {
 			return "", err
 		}
 
-		multiPlatformCmd = "docker buildx inspect --bootstrap"
-		log.Println(" -----> " + multiPlatformCmd)
-		dockerBuildCMD = exec.Command("/bin/sh", "-c", multiPlatformCmd)
-		err = util.RunCommand(dockerBuildCMD)
-		if err != nil {
-			log.Println(err)
-			return "", err
-		}
+		//multiPlatformCmd = "docker buildx inspect --bootstrap"
+		//log.Println(" -----> " + multiPlatformCmd)
+		//dockerBuildCMD = exec.Command("/bin/sh", "-c", multiPlatformCmd)
+		//err = util.RunCommand(dockerBuildCMD)
+		//if err != nil {
+		//	log.Println(err)
+		//	return "", err
+		//}
 	}
 	dest, err := BuildDockerImagePath(ciRequest)
 	if err != nil {
