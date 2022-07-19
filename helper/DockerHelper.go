44896c7b07b59d530b162956d5604e8d8280aa9b
kripanshdevtron <kripansh@devtron.ai>
2022-07-19 12:53:31 +0530

    docker ulimit flag cleanup

--- helper/DockerHelper.go
+++ helper/DockerHelper.go
@@ -57,7 +57,7 @@ func StartDockerDaemon(dockerConnection, dockerRegistryUrl, dockerCert, defaultA
 		defaultAddressPoolFlag = fmt.Sprintf("--default-address-pool base=%s,size=%d", defaultAddressPoolBaseCidr, defaultAddressPoolSize)
 	}
 	if connection == util.INSECURE {
-		dockerdstart = fmt.Sprintf("dockerd  %s --default-ulimit nofile=1024:1024 --insecure-registry %s --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 > /usr/local/bin/nohup.out 2>&1 &", defaultAddressPoolFlag, u.Host)
+		dockerdstart = fmt.Sprintf("dockerd  %s --insecure-registry %s --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 > /usr/local/bin/nohup.out 2>&1 &", defaultAddressPoolFlag, u.Host)
 		util.LogStage("Insecure Registry")
 	} else {
 		if connection == util.SECUREWITHCERT {
@@ -77,7 +77,7 @@ func StartDockerDaemon(dockerConnection, dockerRegistryUrl, dockerCert, defaultA
 			}
 			util.LogStage("Secure with Cert")
 		}
-		dockerdstart = fmt.Sprintf("dockerd %s --default-ulimit nofile=1024:1024 --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 > /usr/local/bin/nohup.out 2>&1 &", defaultAddressPoolFlag)
+		dockerdstart = fmt.Sprintf("dockerd %s --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 > /usr/local/bin/nohup.out 2>&1 &", defaultAddressPoolFlag)
 	}
 	out, _ := exec.Command("/bin/sh", "-c", dockerdstart).Output()
 	log.Println(string(out))
@@ -171,10 +171,10 @@ func BuildArtifact(ciRequest *CiRequest) (string, error) {
 	dockerFileLocationDir := ciRequest.DockerFileLocation[:strings.LastIndex(ciRequest.DockerFileLocation, "/")+1]
 	log.Println(util.DEVTRON, " docker file location: ", dockerFileLocationDir)
 
-	dockerBuild := "docker build --ulimit nofile=1024:1024"
+	dockerBuild := "docker build "
 	useBuildx := ciRequest.DockerBuildTargetPlatform != ""
 	if useBuildx {
-		dockerBuild = "docker buildx build --ulimit nofile=1024:1024 --platform " + ciRequest.DockerBuildTargetPlatform + " "
+		dockerBuild = "docker buildx build --platform " + ciRequest.DockerBuildTargetPlatform + " "
 	}
 	if ciRequest.DockerBuildArgs != "" {
 		dockerBuildArgsMap := make(map[string]string)
