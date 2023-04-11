cc818871f2c8d4439410fec92b302c2c5b62b458
Gireesh Naidu <gireesh@devtron.ai>
2023-04-11 11:32:55 +0530

    build context set in buildx

--- helper/DockerHelper.go
+++ helper/DockerHelper.go
@@ -237,6 +237,9 @@ func BuildArtifact(ciRequest *CiRequest) (string, error) {
 		for key, value := range dockerBuildFlags {
 			dockerBuild = dockerBuild + " " + key + value
 		}
+		if dockerBuildConfig.BuildContext == "" {
+			dockerBuildConfig.BuildContext = "."
+		}
 		if useBuildx {
 			err = installAllSupportedPlatforms()
 			if err != nil {
@@ -257,7 +260,7 @@ func BuildArtifact(ciRequest *CiRequest) (string, error) {
 			}
 			oldCacheBuildxPath = oldCacheBuildxPath + "/cache"
 			manifestLocation := util.LOCAL_BUILDX_LOCATION + "/manifest.json"
-			dockerBuild = fmt.Sprintf("%s -f %s --network host -t %s --push . --cache-to=type=local,dest=%s,mode=max --cache-from=type=local,src=%s --allow network.host --allow security.insecure --metadata-file %s", dockerBuild, dockerBuildConfig.DockerfilePath, dest, localCachePath, oldCacheBuildxPath, manifestLocation)
+			dockerBuild = fmt.Sprintf("%s -f %s --network host -t %s --push %s --cache-to=type=local,dest=%s,mode=max --cache-from=type=local,src=%s --allow network.host --allow security.insecure --metadata-file %s", dockerBuild, dockerBuildConfig.DockerfilePath, dest, dockerBuildConfig.BuildContext, localCachePath, oldCacheBuildxPath, manifestLocation)
 		} else {
 			dockerBuild = fmt.Sprintf("%s -f %s --network host -t %s %s", dockerBuild, dockerBuildConfig.DockerfilePath, ciRequest.DockerRepository, dockerBuildConfig.BuildContext)
 		}
