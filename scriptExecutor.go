beb264ad8f61427efa7c07232cf31eff78292ab8
HimanshuJha2000 <anon99calpha@gmail.com>
2023-06-05 23:42:17 +0530

    writing env values in a different format

--- scriptExecutor.go
+++ scriptExecutor.go
@@ -10,8 +10,12 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sort"
+	"strconv"
 	"strings"
 )
+
+const doubleQuoteSpecialChars = "\\\n\r\"!$`"
 
 func RunScriptsV1(outputPath string, bashScript string, script string, envVars map[string]string) error {
 	log.Println("running script commands")
@@ -178,7 +182,7 @@ func RunScriptsInDocker(executionConf *executionConf) (map[string]string, error)
 		executionConf.EnvInputVars[key] = value
 	}
 
-	err := godotenv.Write(executionConf.EnvInputVars, envInputFileName)
+	err := Write(executionConf.EnvInputVars, envInputFileName)
 	if err != nil {
 		log.Println(util.DEVTRON, err)
 		return nil, err
@@ -248,6 +252,52 @@ func RunScriptsInDocker(executionConf *executionConf) (map[string]string, error)
 	}
 	return envMap, nil
 }
+
+func Write(envMap map[string]string, filename string) error {
+	content, err := Marshal(envMap)
+	if err != nil {
+		return err
+	}
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.WriteString(content + "\n")
+	if err != nil {
+		return err
+	}
+	file.Sync()
+	return err
+}
+
+func Marshal(envMap map[string]string) (string, error) {
+	lines := make([]string, 0, len(envMap))
+	for k, v := range envMap {
+		if d, err := strconv.Atoi(v); err == nil {
+			lines = append(lines, fmt.Sprintf(`%s=%d`, k, d))
+		} else {
+			lines = append(lines, fmt.Sprintf(`%s=%s`, k, doubleQuoteEscape(v)))
+		}
+	}
+	sort.Strings(lines)
+	return strings.Join(lines, "\n"), nil
+}
+
+func doubleQuoteEscape(line string) string {
+	for _, c := range doubleQuoteSpecialChars {
+		toReplace := "\\" + string(c)
+		if c == '\n' {
+			toReplace = `\n`
+		}
+		if c == '\r' {
+			toReplace = `\r`
+		}
+		line = strings.Replace(line, string(c), toReplace, -1)
+	}
+	return line
+}
+
 func buildDockerEntryScript(command string, args []string, outputVars []string) (string, error) {
 	log.Println("output variables are ", outputVars)
 	entryTemplate := `#!/bin/sh
