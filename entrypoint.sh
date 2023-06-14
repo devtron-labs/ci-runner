75580c4f57891c08573ed7ce03a4c22be514355b
Subhashish <subhashish@devtron.ai>
2023-06-14 19:08:09 +0530

    handling sigterm forwarding1

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ set -o pipefail
 if [ "$IN_APP_LOGGING" = "true" ]; then
   # Run cirunner command with logging
 #  exec ./cirunner 2>&1 | tee main.log
-  ./cirunner 2>&1 | { tee main.log & echo $! > cirunner_pid.txt; }
+#  ./cirunner 2>&1 | { tee main.log & echo $! > cirunner_pid.txt; }
+  { ./cirunner 2>&1 & echo $! > cirunner_pid.txt; } | tee main.log
 else
   # Run cirunner command without logging
   exec ./cirunner
@@ -25,11 +26,11 @@ forward_sigterm() {
 # Register the function to handle SIGTERM signal
 trap forward_sigterm SIGTERM
 
-# Wait for the cirunner process to complete
-wait $cirunner_pid
-
-# Perform any additional cleanup if needed
-echo "Script cleanup logic here..."
+## Wait for the cirunner process to complete
+#wait $cirunner_pid
+#
+## Perform any additional cleanup if needed
+#echo "Script cleanup logic here..."
 
 
 
