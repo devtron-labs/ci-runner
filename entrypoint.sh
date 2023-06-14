d9a3e789ad64e5dd892fbec1258ce285711cab5b
Subhashish <subhashish@devtron.ai>
2023-06-14 18:26:09 +0530

    using exec in entrypoint sript

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,19 @@ set -o pipefail
 # Check the value of IN_APP_LOGGING environment variable
 if [ "$IN_APP_LOGGING" = "true" ]; then
   # Run cirunner command with logging
-  ./cirunner 2>&1 | tee main.log
+  exec ./cirunner 2>&1 | tee main.log
 else
   # Run cirunner command without logging
-  ./cirunner
+  exec ./cirunner
 fi
+#
+#cirunner_pid=$!
+#
+## Register a function to forward SIGTERM to the cirunner process
+#forward_sigterm() {
+#  echo "Forwarding SIGTERM to cirunner process..."
+#  kill -SIGTERM $cirunner_pid
+#}
+#
+## Register the function to handle SIGTERM signal
+#trap forward_sigterm SIGTERM
