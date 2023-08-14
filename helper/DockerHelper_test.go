c4d45f91baaaf8634b9f2a1deaf681702d0040b9
Gireesh Naidu <gireesh@devtron.ai>
2023-08-14 12:36:09 +0530

    test case added

--- helper/DockerHelper_test.go
+++ helper/DockerHelper_test.go
@@ -21,3 +21,21 @@ func TestCreateBuildXK8sDriver(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestCleanBuildxK8sDriver(t *testing.T) {
+	buildxOpts := make([]map[string]string, 0)
+	buildxOpts = append(buildxOpts, map[string]string{"node": "", "driverOptions": "namespace=devtron-ci,nodeselector=kubernetes.io/arch:amd64"})
+	buildxOpts = append(buildxOpts, map[string]string{"node": "builder-amd64-test", "driverOptions": "namespace=devtron-ci,nodeselector=kubernetes.io/arch:amd64"})
+	err := createBuildxBuilderWithK8sDriver(buildxOpts)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.Fail()
+	}
+
+	err = CleanBuildxK8sDriver(buildxOpts)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.Fail()
+	}
+
+}
