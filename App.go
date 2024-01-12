49394108d491da16a32a84e8ba5f73c014a7289e
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2024-01-12 12:15:52 +0530

    changed: fixed imports

--- App.go
+++ App.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/aws/aws-sdk-go/aws"
+	ci_cd_stage_executor "github.com/devtron-labs/ci-runner/ci-cd-stage-executor"
 	"github.com/devtron-labs/ci-runner/helper"
 	"github.com/devtron-labs/ci-runner/util"
 	blob_storage "github.com/devtron-labs/common-lib/blob-storage"
@@ -95,8 +96,8 @@ func processEvent(args string) {
 	}
 
 	defer handleCleanup(*ciCdRequest, &exitCode, util.Source_Defer)
-	ciService := ciCdStageExecutor.NewCiStage()
-	cdService := ciCdStageExecutor.NewCdStage()
+	ciService := ci_cd_stage_executor.NewCiStage()
+	cdService := ci_cd_stage_executor.NewCdStage()
 	if ciCdRequest.Type == util.CIEVENT {
 		ciService.HandleCIEvent(ciCdRequest, &exitCode)
 	} else {
