563fb86d71e19f78f666317116561ddb2879243b
prkhrkat <prkhrkat@gmail.com>
2024-05-28 12:59:30 +0530

    error handling for printing the logs even if command fails

--- util/CmdUtil.go
+++ util/CmdUtil.go
@@ -26,7 +26,7 @@ import (
 	"os/exec"
 )
 
-var maskSecrets = false
+var maskSecrets = true
 
 func DeleteFile(path string) error {
 	var err = os.Remove(path)
@@ -39,11 +39,8 @@ func DeleteFile(path string) error {
 func RunCommand(cmd *exec.Cmd) error {
 
 	// Run the command
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Printf("Command execution failed: %v\n", err)
-		return err
-	}
+	output, outputerr := cmd.CombinedOutput()
+
 	outBuf := bytes.NewBuffer(output)
 	if maskSecrets {
 		buf := new(bytes.Buffer)
@@ -51,16 +48,20 @@ func RunCommand(cmd *exec.Cmd) error {
 		maskedStream, err := secretScanner.MaskSecretsStream(outBuf)
 		if err != nil {
 			fmt.Printf("Error masking secrets: %v\n", err)
-			return err
+			fmt.Println(outBuf.String())
 		}
 		_, err = io.Copy(buf, maskedStream)
 		if err != nil {
 			fmt.Printf("Error reading from masked stream: %v\n", err)
-			return err
+			fmt.Println(outBuf.String())
 		}
 		fmt.Println(buf.String())
 	} else {
 		fmt.Println(outBuf.String())
+	}
+	if outputerr != nil {
+		fmt.Printf("Command execution failed: %v\n", outputerr)
+		return outputerr
 	}
 	return nil
 }
